7eed8dcdd9b940840170564d236139447adb5876
Gordon Hecker <gh@emlix.com>
2009-05-14 11:15:53 +0200

    require buildconfig in e2lib
    Signed-off-by: Gordon Hecker <gh@emlix.com>

--- generic/e2lib.lua
+++ generic/e2lib.lua
@@ -25,6 +25,7 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]
 
+require("buildconfig")
 _version = "e2factory, the emlix embedded build system, version " .. 
 							buildconfig.E2_VERSION
 
